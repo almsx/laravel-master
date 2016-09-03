18738ad98ab819de128d0ef2323b170846ebdf60
badillosoft <badillo.soft@hotmail.com>
2016-09-03 11:56:52 -0500

    Sesión 1- Se agregó el poryecto Starbux

--- sites/prueba/routes/web.php
+++ sites/prueba/routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/about", function () {
+    return "Nosotros <strong>somos</strong> ...";
+});
+
+Route::get("user/{id}", function ($id) {
+    return "id: " . $id;
+});
+
+Route::get("saludar/{nombre?}", function ($nombre = "Anónimo") {
+    return "Hola " . $nombre;
+});
+
+Route::get("/usuario/{id?}", function ($id = NULL) {
+    if ($id == NULL) {
+        return "El usuario no existe";
+    }
+
+    $usuarios = [123 => "pepe", 321 => "ash", 465 => "saul"];
+
+    // return "Nombre " . $usuarios[$id];
+    return view("usuario", [ "username" => $usuarios[$id] ]);
+});
---
+++ sites/starbux/resources/views/lista.cafes.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lista de Cafés</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>Tipo</th>
+				<th>Nombre</th>
+				<th>Tamaño</th>
+				<th>Costo</th>
+			</tr>
+		</table>
+	</body>
+</html>
